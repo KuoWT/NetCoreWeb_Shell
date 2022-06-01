1e9d74c9e61eeaa3786ff18333cd038f187127ea
KuoWT <b9715042@gmail.com>
2022-06-01 13:54:46 +0800

    20220601

---
+++ Controllers/OperateController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WMS_API.Models;
+using Microsoft.Extensions.Logging;
+
+namespace WMS_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CostController :ControllerBase
+    {
+
+        IConfiguration _configuration;
+        private readonly ILogger _logger;
+        public CostController(IConfiguration configuration,ILogger<CostController> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        [Route("machinecost/{equip}/{starttime}/{endtime}")]
+        public IActionResult getTotalCost()
+        {
+            return null;
+        }
+    }
+
+}
